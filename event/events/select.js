01134c73fc4d5f9147b3846c4c38a9e8c4b18d0a
bRICK2H <kde@dev.izibook.ru>
2022-08-18 09:18:31 +0300

    sawed out trigger mouseup after select

--- event/events/select.js
+++ event/events/select.js
@@ -1,6 +1,3 @@
-import mouseup from './mouseup'
-import h from '../../helpers/cursor'
-
 export default function (e) {
 	const { target } = e
 		, { pos: { select } } = this
@@ -8,8 +5,4 @@ export default function (e) {
 	select.end = target.selectionEnd
 	select.start = target.selectionStart
 	select.isSelect = select.start !== select.end
-	
-	if (select.isSelect) {
-		mouseup.call(this, e, h)
-	}
 }
